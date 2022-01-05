fcabc6f2327a40d84a7929ea59127dd470045a9c
j-e-g <jeg1986@gmail.com>
2022-01-05 14:17:20 -0300

    Prueba basica de variables y methods.

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
-      mensaje: "hola es un mensaje"
+      titulo: "Prueba de VueJS",
+      vueLink: "https://vuejs.org/",
+      contador:0
+    };
+  },
+
+  methods: {
+    alerta() {
+      alert("hola");
+    },
+    sumar() {
+      this.contador = this.contador + 1;
+    },
+    restar() {
+      this.contador -= 1;
+    },
+    reset() {
+      this.contador = "";
+      this.contador = 0;
     }
   }
 }).mount("#app");
